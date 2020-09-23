61e262aeecd14e1e798f7e7c37380832fc515e7f
Juhwan Song <jsong785@gmail.com>
2020-09-22 21:46:11 -0600

    tweaks

--- monads/monads.go
+++ monads/monads.go
@@ -12,17 +12,17 @@ type ErrorMonad struct {
 }
 type ErrorMonadFunc func(val interface{}) ErrorMonad
 
-func bind(val ErrorMonad, f ...ErrorMonadFunc) ErrorMonad {
+func Bind(val ErrorMonad, f ...ErrorMonadFunc) ErrorMonad {
     if val.err != nil {
         return ErrorMonad{ err: val.err };
     }
     if len(f) == 1 {
         return f[0](val.val)
     }
-    return bind(f[0](val.val), f[1:]...)
+    return Bind(f[0](val.val), f[1:]...)
 }
 
-func AddOne(x interface{}) ErrorMonad {
+func addOne(x interface{}) ErrorMonad {
     number := x.(int)
     if number < 1 {
         return ErrorMonad{ err: errors.New("number must be greater than 0") }
@@ -30,26 +30,26 @@ func AddOne(x interface{}) ErrorMonad {
     return ErrorMonad{ val: number+1 }
 }
 
-func SubtractOne(number interface{}) ErrorMonad {
+func subtractOne(number interface{}) ErrorMonad {
     return ErrorMonad{ val: number.(int) - 1 };
 }
 
-func ToString(number interface{}) ErrorMonad {
+func toString(number interface{}) ErrorMonad {
     return ErrorMonad{ val: strconv.Itoa(number.(int)) }
 }
 
 func main() {
     // success
-    fmt.Println(bind(AddOne(1), AddOne))
-    fmt.Println(bind(AddOne(100), AddOne, AddOne, ToString))
-    fmt.Println(bind(AddOne(1), SubtractOne, AddOne, ToString));
+    fmt.Println(Bind(addOne(1), addOne))
+    fmt.Println(Bind(addOne(100), addOne, addOne, toString))
+    fmt.Println(Bind(addOne(1), subtractOne, addOne, toString));
 
     // errors
-    fmt.Println(bind(AddOne(1), SubtractOne, SubtractOne, AddOne));
-    fmt.Println(bind(AddOne(1), SubtractOne, SubtractOne, AddOne, ToString));
+    fmt.Println(Bind(addOne(1), subtractOne, subtractOne, addOne));
+    fmt.Println(Bind(addOne(1), subtractOne, subtractOne, addOne, toString));
 
-    fmt.Println(bind(AddOne(-1), AddOne))
-    fmt.Println(bind(AddOne(-1), AddOne, AddOne, ToString))
+    fmt.Println(Bind(addOne(-1), addOne))
+    fmt.Println(Bind(addOne(-1), addOne, addOne, toString))
 
 }
 
