93dbd49595bf7ba3ca848dba2f466602d191d5df
Juhwan Song <jsong785@gmail.com>
2020-09-22 22:46:30 -0600

    go fmt

--- channel_pipeline/channel_pipeline.go
+++ channel_pipeline/channel_pipeline.go
@@ -6,59 +6,58 @@ type Channel <-chan interface{}
 type ChannelFunc func(<-chan interface{}, Channel) Channel
 
 func Bind(done <-chan interface{}, input Channel, f ...ChannelFunc) Channel {
-    if input == nil {
-        return nil
-    }
-    if len(f) == 1 {
-        return f[0](done, input)
-    }
-    return Bind(done, f[0](done, input), f[1:]...)
+	if input == nil {
+		return nil
+	}
+	if len(f) == 1 {
+		return f[0](done, input)
+	}
+	return Bind(done, f[0](done, input), f[1:]...)
 }
 
 func multiplyPipe(factor int) ChannelFunc {
-    return func(done <-chan interface{}, input Channel) Channel {
-        c := make(chan interface{})
-        go func() {
-            defer close(c)
-            for x := range input {
-                c <- x.(int) * factor
-            }
-        }()
-        return c
-    }
+	return func(done <-chan interface{}, input Channel) Channel {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			for x := range input {
+				c <- x.(int) * factor
+			}
+		}()
+		return c
+	}
 }
 
 func addPipe(add int) ChannelFunc {
-    return func(done <-chan interface{}, input Channel) Channel {
-        c := make(chan interface{})
-        go func() {
-            defer close(c)
-            for x := range input {
-                c <- x.(int) + add
-            }
-        }()
-        return c
-    }
+	return func(done <-chan interface{}, input Channel) Channel {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			for x := range input {
+				c <- x.(int) + add
+			}
+		}()
+		return c
+	}
 }
 
 func main() {
-    done := make(chan interface{})
-    defer close(done)
+	done := make(chan interface{})
+	defer close(done)
 
+	msg := make(chan interface{})
+	go func() {
+		defer close(msg)
+		for _, i := range []int{1, 2, 3, 4, 5} {
+			msg <- i
+		}
+	}()
 
-    msg:= make(chan interface{})
-    go func() {
-        defer close(msg)
-        for _, i := range []int{1, 2, 3, 4, 5} {
-            msg <- i
-        }
-    }()
-
-    // 2 4 6 8 10
-    // 3 5 7 9 11
-    // 13 15 17 19 21
-    res := Bind(done, msg, multiplyPipe(2), addPipe(1), addPipe(10))
-    for o := range res {
-        fmt.Println(o)
-    }
+	// 2 4 6 8 10
+	// 3 5 7 9 11
+	// 13 15 17 19 21
+	res := Bind(done, msg, multiplyPipe(2), addPipe(1), addPipe(10))
+	for o := range res {
+		fmt.Println(o)
+	}
 }
