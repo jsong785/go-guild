ca7e87ccb27812af138535ddc90f07cefa19ff7f
Juhwan Song <jsong785@gmail.com>
2021-03-08 23:44:21 -0700

    update

--- trie/trie.go
+++ trie/trie.go
@@ -1,75 +1,90 @@
 package trie
 
-
 type Node struct {
-    nodes []Node
-    val byte
-    end bool
-}
-
-func found(nodes []Node, c byte) *Node {
-    for i := 0; i < len(nodes); i++ {
-        if nodes[i].val == c {
-            return &nodes[i]
-        }
-    }
-    return nil
-}
-
-func createNode(val byte) Node {
-    return Node{
-        val: val,
-        end: false,
-    }
-}
-
-func (root *Node) GetNode(val string) *Node {
-    var cur *Node = root
-    for _, c := range val {
-        if n := found(cur.nodes, byte(c)); n != nil {
-            cur = n
-        } else {
-            return nil
-        }
-    }
-    if cur.end {
-        return cur
-    }
-    return nil
-}
-
-func (root *Node) Exists(val string) bool {
-    return root.GetNode(val) != nil
-}
-
-func (root *Node) Delete(val string) bool {
-    var cur *Node = root
-    for _, c := range val {
-        if n := found(cur.nodes, byte(c)); n != nil {
-            cur = n
-        } else {
-            return false;
-        }
-    }
-
-    if cur.end {
-        cur.end = false
-        return true
-    }
-    return false
+	nodes []Node
+	val   byte
+	end   bool
 }
 
 func (root *Node) Insert(val string) *Node {
-    var cur *Node = root
-    for _, c := range val {
-        if n := found(cur.nodes, byte(c)); n != nil {
-            cur = n
-        } else {
-            cur.nodes = append(cur.nodes, createNode(byte(c)))
-            cur = &cur.nodes[len(cur.nodes)-1]
-        }
-    }
-    cur.end = true
-    return cur
+	var current *Node = root
+	for _, c := range val {
+		if found := findByte(current.nodes, byte(c)); found != nil {
+			current = found
+		} else {
+			current.nodes = append(current.nodes, createNode(byte(c)))
+			current = &current.nodes[len(current.nodes)-1]
+		}
+	}
+	current.end = true
+	return current
 }
 
+func (root *Node) Delete(val string) bool {
+	found := root.findNode(val, true)
+	if found != nil {
+		found.end = false
+		return true
+	}
+	return false
+}
+
+func (root *Node) GetNode(val string) *Node {
+	return root.findNode(val, true)
+}
+
+func (root *Node) Exists(val string) bool {
+	return root.GetNode(val) != nil
+}
+
+func (root *Node) GetMatches(val string) []string {
+	n := root.findNode(val, false)
+	if n == nil {
+		return nil
+	}
+	res := make([]string, 0)
+	traverse(n, val[:len(val)-1], func(node *Node, val string) {
+		if node.end {
+			res = append(res, val)
+		}
+	})
+	return res
+}
+
+func (root *Node) findNode(val string, needsEnding bool) *Node {
+	current := root
+	for _, c := range val {
+		if found := findByte(current.nodes, byte(c)); found != nil {
+			current = found
+		} else {
+			return nil
+		}
+	}
+	if !needsEnding || current.end {
+		return current
+	}
+	return nil
+}
+
+func findByte(nodes []Node, c byte) *Node {
+	for i := 0; i < len(nodes); i++ {
+		if nodes[i].val == c {
+			return &nodes[i]
+		}
+	}
+	return nil
+}
+
+func createNode(val byte) Node {
+	return Node{
+		val: val,
+		end: false,
+	}
+}
+
+func traverse(node *Node, current string, nodeFound func(*Node, string)) {
+	nodeFound(node, current+string(node.val))
+	for _, n := range node.nodes {
+		traverse(&n, current+string(node.val), nodeFound)
+	}
+}
--- trie/trie_test.go
+++ trie/trie_test.go
@@ -1,137 +1,170 @@
 package trie
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func Testfound(t *testing.T) {
-    assert.Nil(t, found(createNodes("a"), byte('z')))
-    assert.Nil(t, found(createNodes("a"), byte('z')))
-    assert.Nil(t, found(createNodes("abc"), byte('z')))
+	assert.Nil(t, findByte(createNodes("a"), byte('z')))
+	assert.Nil(t, findByte(createNodes("a"), byte('z')))
+	assert.Nil(t, findByte(createNodes("abc"), byte('z')))
 
-    assert.NotNil(t, found(createNodes("a"), byte('a')))
-    assert.NotNil(t, found(createNodes("abc"), byte('a')))
-    assert.NotNil(t, found(createNodes("abc"), byte('c')))
+	assert.NotNil(t, findByte(createNodes("a"), byte('a')))
+	assert.NotNil(t, findByte(createNodes("abc"), byte('a')))
+	assert.NotNil(t, findByte(createNodes("abc"), byte('c')))
 }
 
 func TestInsert(t *testing.T) {
-    var root Node
-    assert.Nil(t, root.nodes)
+	var root Node
+	assert.Nil(t, root.nodes)
 
-    lNode := root.Insert("val")
-    assert.NotNil(t, lNode)
-    assert.Equal(t, lNode.val, byte('l'))
-    assert.True(t, lNode.end)
-    assert.Nil(t, lNode.nodes)
-    assert.NotNil(t, root.nodes)
+	lNode := root.Insert("val")
+	assert.NotNil(t, lNode)
+	assert.Equal(t, lNode.val, byte('l'))
+	assert.True(t, lNode.end)
+	assert.Nil(t, lNode.nodes)
+	assert.NotNil(t, root.nodes)
 
-    eNode := root.Insert("value")
-    assert.NotNil(t, eNode)
-    assert.Equal(t, eNode.val, byte('e'))
-    assert.True(t, eNode.end)
-    assert.Nil(t, eNode.nodes)
-    assert.NotNil(t, root.nodes)
+	eNode := root.Insert("value")
+	assert.NotNil(t, eNode)
+	assert.Equal(t, eNode.val, byte('e'))
+	assert.True(t, eNode.end)
+	assert.Nil(t, eNode.nodes)
+	assert.NotNil(t, root.nodes)
 
-    assert.NotNil(t, lNode.nodes)
+	assert.NotNil(t, lNode.nodes)
 
-    // ensure lNode goes to eNode
-    assert.Equal(t, lNode.val, byte('l'))
-    assert.True(t, lNode.end)
-    assert.Equal(t, eNode, &lNode.nodes[0].nodes[0])
+	// ensure lNode goes to eNode
+	assert.Equal(t, lNode.val, byte('l'))
+	assert.True(t, lNode.end)
+	assert.Equal(t, eNode, &lNode.nodes[0].nodes[0])
 }
 
 func TestExists(t *testing.T) {
-    var root Node
-    assert.False(t, root.Exists("val"))
+	var root Node
+	assert.False(t, root.Exists("val"))
 
-    root.Insert("value")
-    assert.False(t, root.Exists("val"))
-    assert.False(t, root.Exists("valu"))
-    assert.True(t, root.Exists("value"))
+	root.Insert("value")
+	assert.False(t, root.Exists("val"))
+	assert.False(t, root.Exists("valu"))
+	assert.True(t, root.Exists("value"))
 
-    root.Insert("val")
-    assert.True(t, root.Exists("val"))
-    assert.False(t, root.Exists("valu"))
-    assert.True(t, root.Exists("value"))
+	root.Insert("val")
+	assert.True(t, root.Exists("val"))
+	assert.False(t, root.Exists("valu"))
+	assert.True(t, root.Exists("value"))
 }
 
 func TestDeleteLazy(t *testing.T) {
-    var root Node
-    root.Insert("vaccinate")
-    root.Insert("val")
-    root.Insert("value")
-    assert.True(t, root.Exists("vaccinate"))
-    assert.True(t, root.Exists("val"))
-    assert.True(t, root.Exists("value"))
+	var root Node
+	root.Insert("vaccinate")
+	root.Insert("val")
+	root.Insert("value")
+	assert.True(t, root.Exists("vaccinate"))
+	assert.True(t, root.Exists("val"))
+	assert.True(t, root.Exists("value"))
 
-    root.Delete("what")
-    assert.True(t, root.Exists("vaccinate"))
-    assert.True(t, root.Exists("val"))
-    assert.True(t, root.Exists("value")) 
-    root.Delete("val")
-    assert.True(t, root.Exists("vaccinate"))
-    assert.False(t, root.Exists("val"))
-    assert.True(t, root.Exists("value"))
+	root.Delete("what")
+	assert.True(t, root.Exists("vaccinate"))
+	assert.True(t, root.Exists("val"))
+	assert.True(t, root.Exists("value"))
+	root.Delete("val")
+	assert.True(t, root.Exists("vaccinate"))
+	assert.False(t, root.Exists("val"))
+	assert.True(t, root.Exists("value"))
 
-    root.Delete("v")
-    assert.True(t, root.Exists("vaccinate"))
+	root.Delete("v")
+	assert.True(t, root.Exists("vaccinate"))
 
-    root.Delete("va")
-    assert.True(t, root.Exists("vaccinate"))
+	root.Delete("va")
+	assert.True(t, root.Exists("vaccinate"))
 
-    root.Delete("vac")
-    assert.True(t, root.Exists("vaccinate"))
+	root.Delete("vac")
+	assert.True(t, root.Exists("vaccinate"))
 
-    root.Delete("vacc")
-    assert.True(t, root.Exists("vaccinate"))
+	root.Delete("vacc")
+	assert.True(t, root.Exists("vaccinate"))
 
-    root.Delete("vaccinat")
-    assert.True(t, root.Exists("vaccinate"))
+	root.Delete("vaccinat")
+	assert.True(t, root.Exists("vaccinate"))
 
-    root.Delete("vaccinate")
-    assert.False(t, root.Exists("vaccinate"))
+	root.Delete("vaccinate")
+	assert.False(t, root.Exists("vaccinate"))
 
-    root.Delete("value")
-    assert.False(t, root.Exists("value"))
+	root.Delete("value")
+	assert.False(t, root.Exists("value"))
 }
 
 func TestDeleteLazyThenInsert(t *testing.T) {
-    var root Node
-    root.Insert("val")
-    assert.True(t, root.Exists("val"))
+	var root Node
+	root.Insert("val")
+	assert.True(t, root.Exists("val"))
 
-    root.Delete("val")
-    assert.False(t, root.Exists("val"))
-    
-    root.Insert("val")
-    assert.True(t, root.Exists("val"))
+	root.Delete("val")
+	assert.False(t, root.Exists("val"))
 
-    root.Delete("val")
-    assert.False(t, root.Exists("val"))
+	root.Insert("val")
+	assert.True(t, root.Exists("val"))
 
-    root.Insert("value")
-    assert.False(t, root.Exists("val"))
-    assert.True(t, root.Exists("value"))
+	root.Delete("val")
+	assert.False(t, root.Exists("val"))
+
+	root.Insert("value")
+	assert.False(t, root.Exists("val"))
+	assert.True(t, root.Exists("value"))
 }
 
 func TestRudimentaryForeign(t *testing.T) {
-    var root Node
-    root.Insert("こん")
-    root.Insert("こんにちは")
-    assert.True(t, root.Exists("こん"))
-    assert.True(t, root.Exists("こんにちは"))
+	var root Node
+	root.Insert("こん")
+	root.Insert("こんにちは")
+	assert.True(t, root.Exists("こん"))
+	assert.True(t, root.Exists("こんにちは"))
 
-    assert.True(t, root.Delete("こん"))
-    assert.True(t, root.Delete("こんにちは"))
-    assert.False(t, root.Exists("こんにち"))
+	assert.True(t, root.Delete("こん"))
+	assert.True(t, root.Delete("こんにちは"))
+	assert.False(t, root.Exists("こんにち"))
+}
+
+func TestGetMatches(t *testing.T) {
+	t.Run("simple", func(t *testing.T) {
+		var root Node
+		root.Insert("value")
+		root.Insert("value_bargain")
+
+		res := root.GetMatches("v")
+		assert.Equal(t, []string{"value", "value_bargain"}, res)
+
+		res = root.GetMatches("abc")
+		assert.Nil(t, res)
+	})
+
+	t.Run("complex", func(t *testing.T) {
+		var root Node
+		root.Insert("abc")
+		root.Insert("abacus")
+		root.Insert("dinner")
+		root.Insert("dictionary")
+
+		res := root.GetMatches("a")
+		assert.Equal(t, []string{"abc", "abacus"}, res)
+
+		res = root.GetMatches("abc")
+		assert.Equal(t, []string{"abc"}, res)
+
+		res = root.GetMatches("di")
+		assert.Equal(t, []string{"dinner", "dictionary"}, res)
+
+		res = root.GetMatches("din")
+		assert.Equal(t, []string{"dinner"}, res)
+	})
 }
 
 func createNodes(val string) []Node {
-    nodes := make([]Node, 0)
-    for i, c := range val {
-        nodes = append(nodes, Node{ val: byte(c), end: i == len(val)-1 })
-    }
-    return nodes
+	nodes := make([]Node, 0)
+	for i, c := range val {
+		nodes = append(nodes, Node{val: byte(c), end: i == len(val)-1})
+	}
+	return nodes
 }
-
